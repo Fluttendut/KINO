c5c9d713504582e6c447299b7eef857ec60d3634
Ella Frost <ded_19@hotmail.com>
2023-03-14 13:20:15 +0100

    Changes by micha

--- src/main/java/com/mems/kinozippy/repositories/MovieRepository.java
+++ src/main/java/com/mems/kinozippy/repositories/MovieRepository.java
@@ -4,6 +4,6 @@ import com.mems.kinozippy.entities.Movie;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 
-public class MovieRepository {
+public interface MovieRepository extends JpaRepository<Movie, String> {
 
 }
